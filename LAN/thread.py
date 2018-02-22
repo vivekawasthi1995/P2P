052aba69758533fdb0829ff7524b4f08dd8b9dbf
nkdyo454 <navalkishordewangan454@gmail.com>
2018-02-22 22:57:41 +0530

    Update thread.py
    FIRST MODIFICATION

--- LAN/thread.py
+++ LAN/thread.py
@@ -32,6 +32,15 @@ def another_request(ip, port, mask):
     for i in range( 0, mask):
         s= lambda x: x*x
         
+ def client(ip, port, message):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((ip, port))
+        try:
+            sock.sendall(message)
+            response = sock.recv(1024)
+            print "Received: {}".format(response)
+        finally:
+            sock.close()
 
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
